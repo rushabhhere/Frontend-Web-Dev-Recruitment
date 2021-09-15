ff9ae92eac7e901a7b124155a17dcd46758e876b
Rushabh Javeri <javeri.rushabh45@gmail.com>
2021-09-15 18:24:53 +0530

    Light mode completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,34 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      white: '#FFFFFF',
+      black: '#000000',
+      primary: '#5964E0',
+      'primary-light': '#6771E3',
+      body: '#F4F6F8',
+      dim: '#6E8098',
+    },
+    fontFamily: {
+      sans: [
+        'Kumbh Sans',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'sans-serif',
+      ],
+    },
     borderRadius: {
-      none: '0px',
-      sm: '0.125rem',
       DEFAULT: '0.25rem',
       md: '0.375rem',
-      lg: '0.5rem',
-      xl: '0.75rem',
-      '2xl': '1rem',
-      '3xl': '1.5rem',
       full: '9999px',
       85: '85px',
+    },
+    rotate: {
+      '-35': '-35deg',
+      12: '12deg',
     },
   },
   variants: {
